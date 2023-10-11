744f76f18e58174dcabcfc77e808018eb766f303
Christoph Stock <stockulus@icloud.com>
2023-10-11 08:58:23 +0200

    fix: updtr

--- .eslintrc.js
+++ .eslintrc.js
@@ -74,16 +74,6 @@ module.exports = {
       'error',
       {
         blankLine: 'always',
-        prev: ['const', 'let', 'var'],
-        next: '*'
-      },
-      {
-        blankLine: 'any',
-        prev: ['const', 'let', 'var'],
-        next: ['const', 'let', 'var']
-      },
-      {
-        blankLine: 'always',
         prev: '*',
         next: 'return'
       }
